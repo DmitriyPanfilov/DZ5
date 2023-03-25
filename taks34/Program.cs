3fd504280c19820c5febfc3c792587bd0a944ab2
Dmitriy <Dipan187@live.ru>
2023-03-25 13:03:38 +0300

    Задача 34

---
+++ taks34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] array = new int[4];
+
+void MayArray(int[] collection)
+{
+    int length = collection.Length;
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 1000); // 100 ... 999
+    }
+}
+
+void EvenNumbers(int[] verifiable)
+{
+    int res = 0;
+    int length = verifiable.Length;
+    for(int i = 0; i < length; i++)
+    {
+        
+        if(verifiable[i] % 2 == 0 )
+        res+= 1;
+        
+    }
+    Console.Write($"Четных чисел: {res}");
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for(int j = 0; j < count; j++)
+    {
+        Console.Write($"{col[j]}, ");
+    }
+
+}
+
+MayArray(array);
+PrintArray(array);
+EvenNumbers(array);
+
