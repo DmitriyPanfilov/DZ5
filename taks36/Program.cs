1e9f7a81b3d0b0643bdb016aa78bc0b2b583094d
Dmitriy <Dipan187@live.ru>
2023-03-26 13:21:13 +0300

    Задача 36

---
+++ taks36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[4];
+
+void MayArray(int[] collection)
+{
+    int length = collection.Length;
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-20, 50); // 100 ... 999
+    }
+    
+}
+
+void SumEvenNumbers(int[] verifiable)
+{
+    int sum = 0;
+    int length = verifiable.Length;
+    
+    for(int i = 0; i < length; i++)
+    {
+        int posishon = i + 1;
+        if(posishon % 2 == 1)
+        sum+= verifiable[i];
+    }
+    Console.Write($"сумма чисел на нечетных позициях: {sum}");
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for(int j = 0; j < count; j++)
+    {
+        Console.Write($"{col[j]}, ");
+    }
+
+}
+
+MayArray(array);
+PrintArray(array);
+SumEvenNumbers(array);
